ca9cb14a4604936ec9984a4897002446a3d4c5fd
Frank <scr_0731@126.com>
2013-09-25 19:31:03 +0800

    Initial Commit

---
+++ ObjcDemo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjcDemo
+//
+//  Created by Frank on 9/25/13.
+//  Copyright (c) 2013 THU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
